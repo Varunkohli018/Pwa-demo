9ac065d6e51ed4a6c3652270af267532ac7b0d5b
Varunkohli018 <varunkohli18h@gmail.com>
2025-06-08 18:50:16 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
